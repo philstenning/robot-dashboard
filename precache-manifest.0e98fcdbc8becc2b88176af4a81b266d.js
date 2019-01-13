7ef400d2a044e313ff613afb1f7a017be325e227
philstenning <philstenning@hotmail.com>
2019-01-13 11:22:47 +0000

    Updates

---
+++ precache-manifest.0e98fcdbc8becc2b88176af4a81b266d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "91fb0c9e584ce505e4ea",
+    "url": "/robot-dashboard/static/js/runtime~main.91fb0c9e.js"
+  },
+  {
+    "revision": "29debf7f2b2b6a3cec7f",
+    "url": "/robot-dashboard/static/js/main.29debf7f.chunk.js"
+  },
+  {
+    "revision": "b3d2a6180021747cf95d",
+    "url": "/robot-dashboard/static/js/1.b3d2a618.chunk.js"
+  },
+  {
+    "revision": "29debf7f2b2b6a3cec7f",
+    "url": "/robot-dashboard/static/css/main.a6892b91.chunk.css"
+  },
+  {
+    "revision": "14b420931005aa20bb1fd608a354a036",
+    "url": "/robot-dashboard/index.html"
+  }
+];
